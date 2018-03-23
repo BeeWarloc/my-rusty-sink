ca020f0847eec56e24d7b23efb63a30ecc21e53e
Karsten Nikolai Strand <karsten@fusetools.com>
2018-03-23 18:18:56 +0100

    Just playing around with visitor pattterns

--- tree-lifetimes/src/main.rs
+++ tree-lifetimes/src/main.rs
@@ -1,9 +1,13 @@
 extern crate typed_arena;
 extern crate smallvec;
+extern crate ref_eq;
 
 use std::collections::HashMap;
 use std::fmt;
 use std::cell::RefCell;
+use ref_eq::ref_eq;
+
+mod multirefmap;
 
 #[derive(Copy, Clone, Debug)]
 enum BinOp {
@@ -16,6 +20,59 @@ enum UnOp {
 }
 
 type ExprRef<'a> = &'a Expr<'a>;
+
+trait Visitor<'a, 't: 'a> {
+    fn factory(&self) -> &'a NodeFactory<'a, 't>;
+
+    fn visit_func(&self, func: FuncRef<'a>) -> FuncRef<'a> {
+        unimplemented!()
+    }
+
+    fn visit_expr(&self, node: ExprRef<'a>) -> ExprRef<'a> {
+        self.walk_expr(node)
+    }
+    
+    fn walk_expr(&self, node: ExprRef<'a>) -> ExprRef<'a> {
+        match &node.data {
+            &ExprData::Binary(op, l, r) => {
+                let vl = self.visit_expr(l);
+                let vr = self.visit_expr(r);
+                if ref_eq(l, vl) && ref_eq(r, vr) {
+                    node
+                } else {
+                    self.factory().bin(op, vl, vr)
+                }
+            }
+            &ExprData::Unary(op, n) => {
+                let vn = self.visit_expr(n);
+                if ref_eq(n, vn) {
+                    node
+                } else {
+                    self.factory().unary(op, vn)
+                }
+            }
+            &ExprData::Call(func, ref args) => {
+                let visited_func = self.visit_func(func);
+                let visited_args = self.visit_exprs(args);
+                unimplemented!()
+            }
+            &ExprData::Parameter(parameter) => {
+                node
+            }
+            &ExprData::Integer(_) => {
+                node
+            }
+        }
+    }
+
+    fn visit_parameter(&self, parameter: &'a Parameter<'a>) -> &'a Parameter<'a> {
+        unimplemented!()
+    }
+
+    fn visit_exprs(&self, exprs: &'a Vec<ExprRef<'a>>) -> &'a Vec<ExprRef<'a>> {
+        unimplemented!()
+    }
+}
 
 struct Expr<'a> {
     data: ExprData<'a>,
@@ -33,10 +90,27 @@ impl<'a> std::fmt::Debug for Expr<'a> {
 }
 
 #[derive(Debug)]
+struct Parameter<'a> {
+    name: String,
+    ty: TypeRef<'a>
+}
+
+#[derive(Debug)]
+struct Func<'a> {
+    name: String,
+    body: ExprRef<'a>,
+    parameters: Vec<Parameter<'a>>
+}
+
+type FuncRef<'a> = &'a Func<'a>;
+
+#[derive(Debug)]
 enum ExprData<'a> {
     Integer(u64),
     Binary(BinOp, ExprRef<'a>, ExprRef<'a>),
-    Unary(UnOp, ExprRef<'a>)
+    Unary(UnOp, ExprRef<'a>),
+    Parameter(usize),
+    Call(&'a Func<'a>, Vec<ExprRef<'a>>)
 }
 
 type TypeRef<'t> = &'t Type<'t>;
@@ -83,35 +157,82 @@ impl<'t> TypeContext<'t> {
     }
 }
 
-struct NodeFactory<'a, 't> {
+struct NodeFactory<'a, 't: 'a> {
     nodes: typed_arena::Arena<Expr<'a>>,
     tc: &'t TypeContext<'t>
 }
 
-impl<'a, 't> NodeFactory<'a, 't> where 't: 'a {
+impl<'a, 't> NodeFactory<'a, 't> {
     pub fn new(tc: &'t TypeContext<'t>) -> NodeFactory<'a, 't> {
         NodeFactory {
             nodes: typed_arena::Arena::new(),
             tc
         }
     }
-}
 
-impl<'a, 't> NodeFactory<'a, 't> {
-    pub fn unary(&'a self, op: UnOp, node: ExprRef<'a>) -> ExprRef {
+    pub fn unary(&'a self, op: UnOp, node: ExprRef<'a>) -> ExprRef<'a> {
         self.node(ExprData::Unary(op, node), None)
     }
 
-    pub fn integer(&'a self, value: u64) -> ExprRef {
+    pub fn integer(&'a self, value: u64) -> ExprRef<'a> {
         self.node(ExprData::Integer(value), None)
     }
 
-    pub fn bin(&'a self, op: BinOp, left: ExprRef<'a>, right: ExprRef<'a>) -> ExprRef {
+    pub fn bin(&'a self, op: BinOp, left: ExprRef<'a>, right: ExprRef<'a>) -> ExprRef<'a> {
         self.node(ExprData::Binary(op, left, right), None)
     }
 
     fn node(&'a self, data: ExprData<'a>, ty: Option<TypeRef<'a>>) -> ExprRef<'a> {
         self.nodes.alloc(Expr { data, ty })
+    }
+}
+
+struct NegateVisitor<'a, 't: 'a> {
+    nf: &'a NodeFactory<'a, 't>
+}
+
+impl<'a,'t> Visitor<'a,'t> for NegateVisitor<'a,'t> {
+    fn factory(&self) -> &'a NodeFactory<'a, 't> {
+        self.nf
+    }
+
+    fn visit_expr(&self, node: ExprRef<'a>) -> ExprRef<'a> {
+        let node = self.walk_expr(node);
+        match &node.data {
+            &ExprData::Binary(BinOp::Add, ref l, ref r) => {
+                self.factory().bin(BinOp::Sub, l, r)
+            }
+            &ExprData::Integer(value) => {
+                self.factory().integer(value + 10)
+            }
+            _ => node
+        }
+    }
+}
+
+struct InlineVisitor<'a, 't: 'a> {
+    nf: &'a NodeFactory<'a, 't>,
+    visited: multirefmap::RefCache<'a>
+}
+
+impl<'a,'t> Visitor<'a,'t> for InlineVisitor<'a,'t> {
+    fn factory(&self) -> &'a NodeFactory<'a, 't> {
+        self.nf
+    }
+
+    fn visit_expr(&self, node: ExprRef<'a>) -> ExprRef<'a> {
+        self.visited.get_or_insert(node, ||  {
+            let node = self.walk_expr(node);
+            match &node.data {
+                &ExprData::Binary(BinOp::Add, ref l, ref r) => {
+                    self.factory().bin(BinOp::Sub, l, r)
+                }
+                &ExprData::Integer(value) => {
+                    self.factory().integer(value + 10)
+                }
+                _ => node
+            }
+        })
     }
 }
 
@@ -123,5 +244,8 @@ fn main() {
         let r = f.integer(2);
         let added = f.bin(BinOp::Add, f.integer(1), f.unary(UnOp::Negate, f.integer(222)));
         println!("{:?}", added);
+        let mut visitor = NegateVisitor { nf: &f };
+        let inversed = visitor.visit_expr(added);
+        println!("{:?}", inversed);
     }
 }
